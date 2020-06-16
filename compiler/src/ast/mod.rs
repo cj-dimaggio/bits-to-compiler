882fd3e01893570ee902c796b9237c3a4287da6f
CJ DiMaggio <cjssawa@gmail.com>
2020-06-15 22:58:51 -0400

    Temp

--- compiler/src/ast/mod.rs
+++ compiler/src/ast/mod.rs
@@ -1,22 +1,23 @@
-// #[derive(Debug, PartialEq, Eq)]
-// pub trait Syntax {
-//     type Output;
-//     fn evalute()
-// }
+use super::tokenizer::Token;
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum Expression {
+    BinaryLiteral(u8),
+    StringLiteral(String),
+    NumberLiteral(i64),
+    Arithmetic{
+        first_operand: Box<Expression>,
+        second_operand: Box<Expression>,
+        operator: char,
+    },
+}
 
-// #[derive(Debug, PartialEq, Eq)]
-// pub enum Expression {
-//     BinaryLiteral(u8),
-//     StringLiteral(String),
-//     NumberLiteral(i64),
-//     Arithmetic{
-//         first_operand: Box<Expression>,
-//         second_operand: Box<Expression>,
-//         operator: char,
-//     },
-// }
+pub fn parse(tokens: Vec<Token>) -> Vec<Expression> {
+    let mut ast = Vec::<Expression>::new();
+    let mut cursor = 0;
 
-// pub fn parse() -> Vec<Expression> {
-//     let ast = 
-// }
+    while cursor < tokens.len() {
+    }
+
+    ast
+}
